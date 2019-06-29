a2c4d1ad761f47a82edd269ca22e4e4f722d3539
shad0wrunner <shadowrunner3k@yandex.ru>
2019-06-29 14:15:31 +0300

    Integrated style into the HTML. Changed order of the output.

--- convert_json_results.py
+++ convert_json_results.py
@@ -3,7 +3,7 @@ from json2html import *
 
 
 def transformer(input_json):
-    """ converts input json into output HTML table"""
+    """ converts input json into output HTML table """
     return json2html.convert(json=input_json)
 
 
@@ -25,10 +25,46 @@ def main():
     try:
         # building header of HTML file
         data = '<h1>Docker Security Scanning Report</h1>'
-        data += '<link rel="stylesheet" href="./style.css">'
+
+        # styling the table
+        data += """<style>
+        body {
+          font-family: "Helvetica Neue", Helvetica, Arial;
+          font-size: 14px;
+          line-height: 20px;
+          font-weight: 400;
+          color: #FFFFFF;
+          font-weight: bold;
+          -webkit-font-smoothing: antialiased;
+          font-smoothing: antialiased;
+          background: #2b2b2b;
+        }
+
+        tr {
+          color: #3b3b3b;
+          display: table-row;
+          background: #f6f6f6;
+        }
+
+        tr:nth-of-type(odd) {
+          background: #e9e9e9;
+        }
+
+        th {
+          font-weight: 900;
+          color: #ffffff;
+          background: #ea6153;
+        }
+
+        /* Inner tables */ 
+        td table {
+          font-size: 14px;
+        }
+        </style>
+        """
 
         # getting list of the files to convert to HTML
-        json_files = ['./json/trivy_results.json', './json/dockle_results.json', './json/hadolint_results.json']
+        json_files = ['./json/hadolint_results.json', './json/dockle_results.json', './json/trivy_results.json']
 
         # converting each file and adding some textual info into resulting HTML
         print('[+] Converting JSON results')
@@ -42,6 +78,7 @@ def main():
         print('[+] Writing results HTML')
         write_html('results.html', data)
 
+        print('[+] Clean exit ' + '=' * 60)
     except KeyboardInterrupt:
         print('[X] Exiting ...')
 
