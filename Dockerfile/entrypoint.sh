15c45f1ce4e69152cb410d96281173b1201e6803
Pavel Kann <pkann@swordfishsecurity.com>
2019-07-01 17:49:31 +0300

    included Dockerfile to build scanning image

---
+++ Dockerfile/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "[+] Setting environment variables"
+export SHOWSTOPPER_PRIORITY="CRITICAL"
+export TRIVYCACHE="/.trivy_cache"
+export ARTIFACT_FOLDER="json"
+
+cd docker_tools
+mkdir $ARTIFACT_FOLDER
+
+# Hadolint
+echo "[+] Running Hadolint"
+./hadolint-Linux-x86_64 -f json Dockerfile > $ARTIFACT_FOLDER/hadolint_results.json
+
+# Dockle
+echo "[+] Running Dockle"
+./dockle --exit-code 1 -f json --output $ARTIFACT_FOLDER/dockle_results.json $DOCKERIMAGE
+
+# Trivy
+echo "[+] Running Trivy"
+./trivy --auto-refresh --clear-cache --cache-dir $TRIVYCACHE -f json -o $ARTIFACT_FOLDER/trivy_results.json --exit-code 0 --quiet $DOCKERIMAGE
+./trivy --auto-refresh --cache-dir $TRIVYCACHE --exit-code 1 --severity $SHOWSTOPPER_PRIORITY --quiet $DOCKERIMAGE
+
+# HTML results from all tools outputs
+echo "[+] Making the output look pretty"
+python3 ./convert_json_results.py
+mv $ARTIFACT_FOLDER/*.json /results
+mv results.html /results
+
+# Collect the results in docker_tools/results.html
+echo "[+] Everything is done. Find the resulting HTML report in results.html"
--- Dockerfile/tools_install.sh
+++ Dockerfile/tools_install.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-echo "[+] Setting environment variables"
-export DOCKERFILE="mydockerfile.df"
-export DOCKERIMAGE=" knqyf263/cve-2018-11235"
-export SHOWSTOPPER_PRIORITY="CRITICAL"
-export TRIVYCACHE=".trivy_cache"
-export ARTIFACT_FOLDER="json"
+export TRIVYCACHE="/.trivy_cache"
 
 # installing all necessary stuff
 echo "[+] Installing required packages"
-sudo apt-get update
-sudo apt-get install -y python3 python3-pip docker.io rpm git
+apt-get update
+apt-get install -y rpm wget git python3 python3-pip
 
 # preparing directory structure
 echo "[+] Preparing necessary directories"
@@ -18,39 +13,32 @@ cd docker_tools
 mkdir $TRIVYCACHE
 mkdir $ARTIFACT_FOLDER
 
-# fetching sample Dockerfile
-echo "[+] Fetching sample Dockerfile"
-wget -O $DOCKERFILE https://raw.githubusercontent.com/shad0wrunner/docker_cicd/master/mydockerfile.df
-
 # Hadolint
-echo "[+] Running Hadolint"
+echo "[+] Fetching Hadolint"
 export VERSION=$(wget -q -O - https://api.github.com/repos/hadolint/hadolint/releases/latest | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
 wget -nv --no-cache https://github.com/hadolint/hadolint/releases/download/v${VERSION}/hadolint-Linux-x86_64 -O hadolint-Linux-x86_64 && chmod +x hadolint-Linux-x86_64
-./hadolint-Linux-x86_64 -f json $DOCKERFILE > $ARTIFACT_FOLDER/hadolint_results.json
 
 # Dockle
-echo "[+] Running Dockle"
+echo "[+] Fetching Dockle"
 export VERSION=$(wget -q -O - https://api.github.com/repos/goodwithtech/dockle/releases/latest | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
 wget -nv --no-cache https://github.com/goodwithtech/dockle/releases/download/v${VERSION}/dockle_${VERSION}_Linux-64bit.tar.gz -O dockle_Linux-64bit.tar.gz && tar zxf dockle_Linux-64bit.tar.gz
-./dockle --exit-code 1 -f json --output $ARTIFACT_FOLDER/dockle_results.json $DOCKERIMAGE
 
 # Trivy
-echo "[+] Running Trivy"
+echo "[+] Fetching Trivy"
 export VERSION=$(wget -q -O - https://api.github.com/repos/knqyf263/trivy/releases/latest | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
 wget -nv --no-cache https://github.com/knqyf263/trivy/releases/download/v${VERSION}/trivy_${VERSION}_Linux-64bit.tar.gz -O trivy_Linux-64bit.tar.gz && tar zxf trivy_Linux-64bit.tar.gz
 
-./trivy --auto-refresh --clear-cache --cache-dir $TRIVYCACHE -f json -o $ARTIFACT_FOLDER/trivy_results.json --exit-code 0 --quiet $DOCKERIMAGE
-./trivy --auto-refresh --cache-dir $TRIVYCACHE --exit-code 1 --severity $SHOWSTOPPER_PRIORITY --quiet $DOCKERIMAGE
+echo "[+] Fetching Trivy DB"
+./trivy --refresh -q --cache-dir $TRIVYCACHE
 
 # cleaning up
 echo "[+] Removing left-overs"
 rm *.tar.gz LICENSE README.md 
 
 # HTML results from all tools outputs
-echo "[+] Making the output look pretty"
+echo "[+] Fetching json2HTML"
 pip3 install json2html
 wget -nv --no-cache -O convert_json_results.py https://raw.githubusercontent.com/shad0wrunner/docker_cicd/master/convert_json_results.py
-python3 ./convert_json_results.py
 
 # Collect the results in docker_tools/results.html
-echo "[+] Everything is done. Find the resulting HTML report in results.html"
+echo "[+] Image has been built"
