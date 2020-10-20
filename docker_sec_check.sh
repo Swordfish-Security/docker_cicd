80ddda5b0a7c920f1b60c75692f7074946e10c3d
shad0wrunner <shadowrunner3k@gmail.com>
2020-10-20 13:50:20 +0300

    Updating output and parameters
    Removed unnecessary Trivy parameters
    Added more user-friendly output for those who (like me) can't read json

--- docker_sec_check.sh
+++ docker_sec_check.sh
@@ -35,23 +35,35 @@ export VERSION=$(wget -q -O - https://api.github.com/repos/hadolint/hadolint/rel
 wget -nv --no-cache https://github.com/hadolint/hadolint/releases/download/v${VERSION}/hadolint-Linux-x86_64 -O hadolint-Linux-x86_64 && chmod +x hadolint-Linux-x86_64
 ./hadolint-Linux-x86_64 -f json $DOCKERFILE > $ARTIFACT_FOLDER/hadolint_results.json
 
+# show results
+./hadolint-Linux-x86_64 $DOCKERFILE
+
 # Dockle
 echo "[+] Running Dockle"
 export VERSION=$(wget -q -O - https://api.github.com/repos/goodwithtech/dockle/releases/latest | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
 wget -nv --no-cache https://github.com/goodwithtech/dockle/releases/download/v${VERSION}/dockle_${VERSION}_Linux-64bit.tar.gz -O dockle_Linux-64bit.tar.gz && tar zxf dockle_Linux-64bit.tar.gz
 ./dockle --exit-code 1 -f json --output $ARTIFACT_FOLDER/dockle_results.json $DOCKERIMAGE
 
+# show results
+./dockle $DOCKERIMAGE
+
 # Trivy
 echo "[+] Running Trivy"
 export VERSION=$(wget -q -O - https://api.github.com/repos/knqyf263/trivy/releases/latest | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
 wget -nv --no-cache https://github.com/knqyf263/trivy/releases/download/v${VERSION}/trivy_${VERSION}_Linux-64bit.tar.gz -O trivy_Linux-64bit.tar.gz && tar zxf trivy_Linux-64bit.tar.gz
 
-./trivy --auto-refresh --clear-cache --cache-dir $TRIVYCACHE -f json -o $ARTIFACT_FOLDER/trivy_results.json --exit-code 0 --quiet $DOCKERIMAGE
-./trivy --auto-refresh --cache-dir $TRIVYCACHE --exit-code 1 --severity $SHOWSTOPPER_PRIORITY --quiet $DOCKERIMAGE
+# writing finding into json file
+./trivy --cache-dir $TRIVYCACHE -f json -o $ARTIFACT_FOLDER/trivy_results.json --exit-code 0 --quiet $DOCKERIMAGE
+
+# just a neat output instead of pure json
+./trivy --cache-dir $TRIVYCACHE --exit-code 0 $DOCKERIMAGE
+
+# fail build if there is at least 1 vulnerability of the defined severity
+./trivy -d --cache-dir $TRIVYCACHE --exit-code 1 --severity $SHOWSTOPPER_PRIORITY --quiet $DOCKERIMAGE
 
 # cleaning up
 echo "[+] Removing left-overs"
-rm *.tar.gz LICENSE README.md 
+rm *.tar.gz LICENSE README.md
 
 # HTML results from all tools outputs
 echo "[+] Making the output look pretty"
