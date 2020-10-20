c3c8c6fc4a126e6bd2f199d4c5a6fbffa82eab4c
shad0wrunner <shadowrunner3k@gmail.com>
2020-10-20 15:07:25 +0300

    Update entrypoint.sh

--- Dockerfile_tar/entrypoint.sh
+++ Dockerfile_tar/entrypoint.sh
@@ -11,14 +11,26 @@ mkdir $ARTIFACT_FOLDER
 echo "[+] Running Hadolint"
 ./hadolint-Linux-x86_64 -f json /Dockerfile > $ARTIFACT_FOLDER/hadolint_results.json
 
+# show results
+./hadolint-Linux-x86_64 /Dockerfile
+
 # Dockle
 echo "[+] Running Dockle"
 ./dockle --exit-code 1 -f json --output $ARTIFACT_FOLDER/dockle_results.json --input /$DOCKERIMAGE
 
+# show results
+./dockle --input /$DOCKERIMAGE
+
 # Trivy
 echo "[+] Running Trivy"
-./trivy --auto-refresh --clear-cache --cache-dir $TRIVYCACHE -f json -o $ARTIFACT_FOLDER/trivy_results.json --exit-code 0 --quiet --input /$DOCKERIMAGE
-./trivy --auto-refresh --cache-dir $TRIVYCACHE --exit-code 1 --severity $SHOWSTOPPER_PRIORITY --quiet --input /$DOCKERIMAGE
+# writing finding into json file
+./trivy --cache-dir $TRIVYCACHE -f json -o $ARTIFACT_FOLDER/trivy_results.json --exit-code 0 --quiet --input /$DOCKERIMAGE
+
+# just a neat output instead of pure json
+./trivy --cache-dir $TRIVYCACHE --exit-code 0 --input /$DOCKERIMAGE
+
+# fail build if there is at least 1 vulnerability of the defined severity
+./trivy -d --cache-dir $TRIVYCACHE --exit-code 1 --severity $SHOWSTOPPER_PRIORITY --quiet --input /$DOCKERIMAGE
 
 # HTML results from all tools outputs
 echo "[+] Making the output look pretty"
